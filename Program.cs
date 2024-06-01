fb0a17eb9c9e145122ffbf349cb232cb7f3213b6
MichaelHinrichs <mchinrichs@gmail.com>
2024-06-01 13:36:25 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//Written for LuckCatchers games.
+//LuckCatchers https://store.steampowered.com/app/454540
+//LuckCatchers 2 https://store.steampowered.com/app/2700970
+using System.IO;
+
+namespace LuckCatchers_Extractor
+{
+    class Program
+    {
+        public static BinaryReader br;
+        static void Main(string[] args)
+        {
+            br = new(File.OpenRead(args[0]));
+            br.BaseStream.Position = 8;
+
+            System.Collections.Generic.List<Subfile> subfiles = new();
+            subfiles.Add(new());
+            for(int i = 0; i < subfiles[0].size - 1; i++)
+                subfiles.Add(new());
+
+            br.ReadInt32();//unknown
+            long dataStart = br.BaseStream.Position;
+            string path = Path.GetDirectoryName(args[0]) + "//";
+
+            foreach (Subfile file in subfiles)
+            {
+                if (file.isFolder == 1)
+                    Directory.CreateDirectory(path + file.path);
+                else
+                {
+                    br.BaseStream.Position = file.start + dataStart;
+                    BinaryWriter bw = new(File.Create(path + file.path));
+                    bw.Write(br.ReadBytes(file.size));
+                    bw.Close();
+                }
+            }
+        }
+
+        class Subfile
+        {
+            public int size = br.ReadInt32();
+            public byte isFolder = br.ReadByte();
+            public string name = new string(br.ReadChars(br.ReadInt32())).TrimEnd('\0');
+            public string path = new string(br.ReadChars(br.ReadInt32())).TrimEnd('\0');
+            public int start = br.ReadInt32();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LuckCatchers-Extractor
+ A tool to extract from RES files found in [LuckCatchers](https://store.steampowered.com/app/454540) and [LuckCatchers 2](https://store.steampowered.com/app/2700970).
